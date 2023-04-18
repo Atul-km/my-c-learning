eb0ac3dc182207d908b56103efae0f71be2f7955
Atul-km <130464412+Atul-km@users.noreply.github.com>
2023-04-18 15:34:50 +0530

    Update hcf.c

--- loops/while loop/hcf.c
+++ loops/while loop/hcf.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
+int main(){
+    int numOne , numTwo;
 
+    printf("enter two nos two find hcf and lcm\n");
+    scanf("%d %d",&numOne,&numTwo);
+
+    //hcf logic by euclid 
+   
+    while (numTwo != 0 ){
+        int  remainder = numOne%numTwo;
+        numOne= numTwo;
+        numTwo =  remainder;
+        
+    }
+   printf("HCF is %d \n",numOne);
+   
+}
